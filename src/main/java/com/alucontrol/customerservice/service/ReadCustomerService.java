b991074a2084314d546173f0a0e4310d54d53fde
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-18 19:08:59 +0000

    Create ReadCustomerService.java
    two methods created
    
    act:
    -> find by id
    
    -> find all

---
+++ src/main/java/com/alucontrol/customerservice/service/ReadCustomerService.java
@@ -0,0 +1,34 @@
+package com.alucontrol.customerservice.service;
+
+import com.alucontrol.customerservice.entity.Customer;
+import com.alucontrol.customerservice.exceptions.ResourceNotFoundException;
+import com.alucontrol.customerservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReadCustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public ReadCustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer findCustomerById(Long id) {
+
+        Optional<Customer> customer = customerRepository.findById(id);
+        if (customer.isPresent()) {
+            return customer.get();
+        }
+        throw new ResourceNotFoundException("Cliente id '" + id + "' não encontrado");
+    }
+
+    public List<Customer> findAllCustomer() {
+        return customerRepository.findAll();
+    }
+}
