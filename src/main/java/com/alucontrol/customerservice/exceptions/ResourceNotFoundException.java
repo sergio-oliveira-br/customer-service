8077fd9b568e98b4d1c97958808d5228c967a0cd
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-18 18:26:38 +0000

    Create ResourceNotFoundException.java

---
+++ src/main/java/com/alucontrol/customerservice/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.alucontrol.customerservice.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
